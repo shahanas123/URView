d369f4e9903ca4d430f54b63be3bb0f60aff6404
shahanas123 <mca545@rajagiri.edu>
2021-03-19 22:00:37 +0530

    push

---
+++ index.php
@@ -0,0 +1,102 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="http://gfdrr.github.io/innovation-lab-brand-guidelines/images/01-brand-guidelines/01-07-download-resources/1-7-e5.png" class="img-thumbnail" alt="...">View</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="movies.php">Movies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="series.php">series</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="songs.php">songs</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Profile
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Account</a></li>
+              <li><a class="dropdown-item" href="#">Downloads</a></li>
+              <li><a class="dropdown-item" href="#">subscription</a></li>
+              <li><a class="dropdown-item" href="#">settings</a></li>
+            </ul>
+          </li>
+         
+        </ul>
+        <form class="d-flex">
+
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+          
+        </form>
+        <nav class="navbar navbar-light bg-light">
+          <form class="container-fluid justify-content-start">
+            <a class="btn btn-outline-success me-2" href="signin.php" >Sign in</a>
+            
+          </form>
+        </nav>
+      </div>
+    </div>
+  </nav>
+<body>
+    <div class="container">
+        <div class="row">
+            <img height="300px" src="https://img.etimg.com/thumb/width-1200,height-900,imgsize-413727,resizemode-1,msid-78810225/industry/media/entertainment/media/media-entertainment-sector-to-grow-over-10-percent-till-2024-report.jpg" class="d-block w-100" alt="...">
+
+        </div>
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px"src="https://www.discoverboating.com/sites/default/files/inline-images/best-boat-movies.png" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="movies.php" class="card-link">Movies</a>
+                    </div>
+                </div>
+                  
+            </div>
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px"src="https://netstorage-yen.akamaized.net/images/f6f2e7d16f4705b1.jpg?imwidth=1200&impolicy=default-amp" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="series.php" class="card-link">Series</a>
+                    </div>
+                </div>
+                  
+            </div>
+           
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px" src="https://www.udiscovermusic.com/wp-content/uploads/2020/12/songs-that-defined-2020.jpg" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="songs.php" class="card-link">Songs</a>
+                    </div>
+                </div>
+            
+        
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
---
+++ series.php
@@ -0,0 +1,91 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-BmbxuPwQa2lc/FVzBcNJ7UAyJxM6wuqIj61tLrc4wSX0szH/Ev+nYRRuWlolflfl" crossorigin="anonymous">
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<nav class="navbar navbar-expand-lg navbar-light bg-light">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="#"><img src="http://gfdrr.github.io/innovation-lab-brand-guidelines/images/01-brand-guidelines/01-07-download-resources/1-7-e5.png" class="img-thumbnail" alt="...">View</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="index.php">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="movies.php">Movies</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="series.php">series</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="songs.php">songs</a>
+          </li>
+          <li class="nav-item dropdown">
+            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              Profile
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+              <li><a class="dropdown-item" href="#">Account</a></li>
+              <li><a class="dropdown-item" href="#">Downloads</a></li>
+              <li><a class="dropdown-item" href="#">subscription</a></li>
+              <li><a class="dropdown-item" href="#">settings</a></li>
+            </ul>
+          </li>
+         
+        </ul>
+        <form class="d-flex">
+          <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search">
+          <button class="btn btn-outline-success" type="submit">Search</button>
+        </form>
+      </div>
+    </div>
+  </nav>
+<body>
+    <div class="container">
+        
+        <div class="row">
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px"src="https://lh3.googleusercontent.com/proxy/-2kewD4Ef3PJKkNggx3qTMyapYDu-fb19DDv_jHWskbV0NIHiyvGax7vMb_FdUF2KYRhjKlhqWc-5TjTqtQOdC7V8B4MTO4jbZsOU_k2ae2qAcJenLGuJkRn1hw9hw" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="movies.php" class="card-link">English</a>
+                    </div>
+                </div>
+                  
+            </div>
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px"src="https://i0.wp.com/travelwithkarla.com/wp-content/uploads/2017/12/best-korean-dramas-in-2017.png?resize=838%2C480&ssl=1" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="series.php" class="card-link">Korean</a>
+                    </div>
+                </div>
+                  
+            </div>
+           
+            <div class="col col-12 col-sm-12 col-md-4 col-lg-4 col-xl-4 col-xxl-4">
+                <div class="card" style="width: 18rem;">
+                    <img height="250px" src="https://i.pinimg.com/originals/d4/73/87/d47387710d812dc3a58e43e49ec0e91f.jpg" class="card-img-top" alt="...">
+                    
+                    <div class="card-body">
+                      
+                      <a href="songs.php" class="card-link">Hindi</a>
+                    </div>
+                </div>
+            
+        
+    </div>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta2/dist/js/bootstrap.bundle.min.js" integrity="sha384-b5kHyXgcpbZJO/tY9Ul7kGkf1S0CWuKcCD38l8YkeH8z8QjE0GmW1gYU5S9FOnJ0" crossorigin="anonymous"></script>
+</body>
+</html>
